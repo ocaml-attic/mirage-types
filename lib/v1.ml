d0e1576ca6ca41893e86083b84fdc10206aaacc3
Anil Madhavapeddy <anil@recoil.org>
2013-11-07 16:48:08 -0800

    Add module type CLOCK

--- lib/v1.ml
+++ lib/v1.ml
@@ -60,3 +60,30 @@ module type IO_PAGE = sig
   (** [round_to_page_size n] returns the number of bytes that will be
       allocated for storing [n] bytes in memory *)
 end
+
+module type CLOCK = sig
+  (** Clock operations.
+      Currently read-only to retrieve the time in various formats. *)
+
+  type tm =
+    { tm_sec : int;               (** Seconds 0..60 *)
+      tm_min : int;               (** Minutes 0..59 *)
+      tm_hour : int;              (** Hours 0..23 *)
+      tm_mday : int;              (** Day of month 1..31 *)
+      tm_mon : int;               (** Month of year 0..11 *)
+      tm_year : int;              (** Year - 1900 *)
+      tm_wday : int;              (** Day of week (Sunday is 0) *)
+      tm_yday : int;              (** Day of year 0..365 *)
+      tm_isdst : bool;            (** Daylight time savings in effect *)
+    }
+  (** The type representing wallclock time and calendar date. *)
+
+  val time : unit -> float
+  (** Return the current time since 00:00:00 GMT, Jan. 1, 1970, in
+      seconds. *)
+
+  val gmtime : float -> tm
+  (** Convert a time in seconds, as returned by {!time}, into a
+      date and a time. Assumes UTC (Coordinated Universal Time), also
+      known as GMT. *)
+end
